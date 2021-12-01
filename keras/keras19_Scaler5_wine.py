b6273d91f0d2a8bda64398dfce3bbe5e3e083b07
NamKungJiHee <94839062+NamKungJiHee@users.noreply.github.com>
2021-12-01 20:37:26 +0900

    Add files via upload

---
+++ keras/keras19_Scaler5_wine.py
@@ -0,0 +1,145 @@
+from sklearn.datasets import load_wine
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
+from sklearn.model_selection import train_test_split
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#1) 데이터
+datasets = load_wine()
+
+x = datasets.data
+y = datasets.target
+
+#print(x.shape, y.shape)  # (178, 13) (178,)
+#print(y)
+#print(np.unique(y))  
+from tensorflow.keras.utils import to_categorical
+y = to_categorical(y)   #One Hot Encoding
+print(y)
+print(y.shape)   # (178, 3) 
+
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True, random_state=66)
+
+#scaler = MinMaxScaler()
+#scaler = StandardScaler()
+#scaler = RobustScaler()
+scaler = MaxAbsScaler()
+
+scaler.fit(x_train)  # 어느 비율로 나눌지
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test) #x_train에 맞는 비율로 들어가있움
+
+
+
+#2) 모델구성
+model = Sequential()
+model.add(Dense(10, activation = 'linear', input_dim = 13))     
+model.add(Dense(30, activation = 'linear'))
+model.add(Dense(40, activation= 'relu'))
+model.add(Dense(50, activation= 'relu'))
+model.add(Dense(3, activation = 'softmax'))
+
+
+#model.summary()
+
+#3) 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])   # accuracy는 훈련에 영향 안끼친당
+
+#es = EarlyStopping(monitor = 'val_loss', patience = 10, mode = 'min', verbose=1, restore_best_weights=True) 
+
+model.fit(x_train, y_train, epochs=100, batch_size=1, verbose=1, validation_split=0.2) #callbacks=[es])
+
+#4) 평가, 예측
+
+
+loss=model.evaluate(x_test, y_test)
+  
+print('loss: ', loss[0]) 
+print('accuracy: ', loss[1])
+
+'''
+results=model.predict(x_test[:7])
+print(y_test[:7])  
+print(results)
+'''
+
+"""
+# 결과       loss/accuracy
+# 그냥
+loss:  0.2890169620513916
+accuracy:  0.8333333134651184
+
+# MinMAX
+loss:  0.4935479164123535
+accuracy:  0.9722222089767456
+
+#Standard
+loss:  0.43347129225730896
+accuracy:  0.9722222089767456
+
+# robust 
+loss:  0.27580058574676514
+accuracy:  0.9722222089767456
+
+# maxabs
+loss:  0.05323462560772896
+accuracy:  0.9722222089767456
+
+== 
+maxabs scaler로 돌렸을 때 loss의 값이 가장 낮게 나옴! 
+
+##################################### relu 적용 후 
+# 그냥
+loss:  0.5824667811393738
+accuracy:  0.7222222089767456
+
+# MinMAX
+loss:  0.20618239045143127
+accuracy:  0.9722222089767456
+
+#Standard
+loss:  0.05545978248119354
+accuracy:  0.9722222089767456
+
+# robust 
+loss:  0.2698124647140503
+accuracy:  0.9722222089767456
+
+# maxabs
+loss:  0.01699216477572918
+accuracy:  1.0
+
+== wine의 자료의 경우) relu 함수를 쓴 결과 전체적으로 loss의 값이 조아졌다! (그냥 돌렸을 때 빼고!)
+
+######################summary
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+dense (Dense)                (None, 10)                140
+_________________________________________________________________
+dense_1 (Dense)              (None, 30)                330
+_________________________________________________________________
+dense_2 (Dense)              (None, 40)                1240
+_________________________________________________________________
+dense_3 (Dense)              (None, 50)                2050
+_________________________________________________________________
+dense_4 (Dense)              (None, 3)                 153
+=================================================================
+Total params: 3,913
+Trainable params: 3,913
+Non-trainable params: 0
+
+
+
+
+
+
+
+
+
+
+
+"""
+
---
+++ keras/keras19_Scaler6_fetch_covtype.py
@@ -0,0 +1,109 @@
+from sklearn.datasets import fetch_covtype
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, MaxAbsScaler
+from sklearn.model_selection import train_test_split
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+datasets = fetch_covtype()
+
+x = datasets.data
+y = datasets.target
+
+#print(x.shape, y.shape)   # (581012, 54) (581012,) 
+#print(np.unique(y))
+
+import pandas as pd   
+y = pd.get_dummies(y)   
+# print(y.shape)  
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, shuffle=True, random_state=66)
+
+#scaler = MinMaxScaler()
+#scaler = StandardScaler()
+#scaler = RobustScaler()
+scaler = MaxAbsScaler()
+
+scaler.fit(x_train)  
+x_train = scaler.transform(x_train)
+x_test = scaler.transform(x_test)
+
+#2) 모델링
+model = Sequential()
+model.add(Dense(10, activation= 'relu', input_dim = 54))
+model.add(Dense(10))
+model.add(Dense(10, activation = 'relu'))
+model.add(Dense(10))
+model.add(Dense(7, activation = 'softmax'))
+
+#model.summary()
+
+#3) 캄파일, 훈련
+
+model.compile(loss= 'categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])
+
+# es = EarlyStopping(monitor='val_loss', patience=50, mode='min', verbose=1, restore_best_weights=True)
+
+model.fit(x_train, y_train, epochs = 100, batch_size=500, validation_split= 0.2) #callbacks=[es])  
+
+#4) 평가, 예측
+
+loss= model.evaluate(x_test, y_test)
+print('loss: ', loss)
+
+"""
+# 결과       loss/accuracy
+# 그냥
+loss:  [0.6471015214920044, 0.7202911972999573]
+
+# MinMAX
+loss:  [0.6307290196418762, 0.7230794429779053]
+
+#Standard
+loss:  [0.6301481127738953, 0.7230364084243774]
+
+# robust 
+loss:  [0.6297274231910706, 0.7247231006622314]
+
+# maxabs
+loss:  [0.630656898021698, 0.7239227890968323]
+
+== 왜 다 비슷비슷하게 나오징??
+
+##################################### relu 적용 후 
+# 그냥
+loss:  [0.6213434934616089, 0.7356264591217041]
+
+# MinMAX
+loss:  [0.5480517148971558, 0.7643434405326843]
+
+#Standard
+loss:  [0.5432329773902893, 0.7684139013290405]
+
+# robust 
+loss:  [0.527684211730957, 0.7767355442047119]
+
+# maxabs
+loss:  [0.5639169812202454, 0.7571319341659546]
+
+== relu함수를 쓰니 그 이전결과보다 loss값이 조아졌음을 볼 수 있따!
+
+######################summary
+
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+dense (Dense)                (None, 10)                550
+_________________________________________________________________
+dense_1 (Dense)              (None, 10)                110
+_________________________________________________________________
+dense_2 (Dense)              (None, 10)                110
+_________________________________________________________________
+dense_3 (Dense)              (None, 10)                110
+_________________________________________________________________
+dense_4 (Dense)              (None, 7)                 77
+=================================================================
+Total params: 957
+Trainable params: 957
+Non-trainable params: 0
+"""
