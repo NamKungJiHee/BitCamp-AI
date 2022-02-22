e1e0cf3ed2fae928bfc56b15a7b135aaf92ee9d7
NamKungJiHee <94839062+NamKungJiHee@users.noreply.github.com>
2022-02-22 11:39:25 +0900

    02/22

---
+++ keras2/keras70_02_cifar100_Xception.py
@@ -0,0 +1,2 @@
+## Error ##
+# ValueError: Input size must be at least 71x71; Received: input_shape=(32, 32, 3) #
---
+++ keras2/keras70_06_cifar100_InceptionResNetV2.py
@@ -0,0 +1,2 @@
+##  Error ##
+# ValueError: Input size must be at least 75x75; got `input_shape=(32, 32, 3)`#
