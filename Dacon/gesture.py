7bff8b56c08cd886028feffa5f7a845acc2cdb14
Judy <94839062+NamKungJiHee@users.noreply.github.com>
2022-03-17 15:24:52 +0900

    03/17

---
+++ Dacon/gesture.py
@@ -0,0 +1,165 @@
+# Basic Library
+import pandas as pd
+import numpy as np
+import os
+
+# Preprocessing
+from sklearn.preprocessing import OneHotEncoder
+
+# HP Tuning
+import optuna
+from optuna import Trial
+from optuna.samplers import TPESampler
+from optuna.visualization import plot_contour, plot_optimization_history
+from optuna.visualization import plot_parallel_coordinate, plot_slice, plot_param_importances
+
+# Modeling
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import accuracy_score, log_loss
+import tensorflow as tf
+import keras
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Dropout, Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+path = './_data/dacon/gesture/'
+
+train = pd.read_csv(path + "./train.csv")
+test = pd.read_csv(path + "./test.csv")
+submission = pd.read_csv(path + "./sample_submission.csv")
+
+train_x = train.iloc[:, 1:-1]
+test_x = test.iloc[:, 1:]
+
+train_x = np.array(train_x).reshape(-1, 8, 4, 1)
+test_x = np.array(test_x).reshape(-1, 8, 4, 1)
+
+ohe = OneHotEncoder(sparse = False)
+train_y = ohe.fit_transform(train[['target']])
+
+def create_model(num_layer, mid_units, num_filters):
+    
+    model = Sequential()
+    model.add(Conv2D(filters=num_filters[0], kernel_size=(2, 2),
+                 activation="relu",
+                 input_shape=(8, 4, 1)))
+    model.add(BatchNormalization())
+    #model.add(Dropout(dropout_rate[0]))
+    for i in range(1,num_layer):
+        model.add(Conv2D(filters=num_filters[i], kernel_size=(3, 3), padding="same", activation="relu"))
+        model.add(BatchNormalization())
+        #model.add(Dropout(dropout_rate[i+1]))
+            
+    model.add(GlobalAveragePooling2D())
+    model.add(Dense(mid_units, activation='relu'))
+    #model.add(Dropout(dropout_rate[-1]))
+    model.add(Dense(4, activation='softmax'))
+    
+    return model
+
+def cnn_objective(trial: Trial) -> float:
+    
+    #clear_session
+    keras.backend.clear_session()
+    
+    #number of the convolution layer
+    num_layer = trial.suggest_int("num_layer", 2, 3)
+    
+    #number of the unit
+    mid_units = int(trial.suggest_discrete_uniform("mid_units", 30, 150, 10))
+    
+    #number of the each convolution layer filter
+    num_filters = [int(trial.suggest_discrete_uniform("num_filter_"+str(i), 16, 256, 16)) for i in range(num_layer)]
+
+    #Dropout
+    #dropout_rate = trial.suggest_uniform('dropout_rate', 0.0, 0.5)
+    #dropout_rate = [int(trial.suggest_uniform("dropout_rate"+str(ii), 0.0, 0.5)) for ii in range(num_layer+1)]
+    
+        
+    seed = 47
+    kfold = StratifiedKFold(n_splits=8, random_state = seed, shuffle = True) # Cross-validation cv=5
+    es = EarlyStopping(monitor="val_acc", patience=15, mode="max", verbose=0)
+    cv = np.zeros((train_x.shape[0], 4))
+
+    for n, (train_idx, val_idx) in enumerate(kfold.split(train_x, train.target)):
+
+        x_train, y_train = train_x[train_idx], train_y[train_idx]
+        x_val, y_val = train_x[val_idx], train_y[val_idx]
+        
+        mc = ModelCheckpoint(f"model_{n+1}.h5", save_best_only=True, monitor="val_acc", mode="max", verbose=0)
+        
+        model = create_model(num_layer, mid_units, num_filters)
+        
+        # Adam optimizer learning rate
+        optimizer = Adam(learning_rate=trial.suggest_uniform("learning_rate", 0.0005, 0.005))
+        model.compile(optimizer=optimizer,
+                      loss="categorical_crossentropy",
+                      metrics=["acc"])
+        model.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=100, batch_size=40, 
+                  callbacks=[es,mc], verbose=None)
+        
+        best = load_model(f"model_{n+1}.h5")
+        
+        cv[val_idx, :] = best.predict(x_val)
+        
+    print('multi_logloss:', log_loss(train_y, cv))
+    print('accuracy_score:', accuracy_score(np.argmax(train_y, axis=1), np.argmax(cv, axis=1)))
+
+    
+    return accuracy_score(np.argmax(train_y, axis=1), np.argmax(cv, axis=1))
+
+sampler = TPESampler(seed=47)
+cnn_study = optuna.create_study(study_name="cnn_parameter_opt", direction="maximize", sampler=sampler)
+cnn_study.optimize(cnn_objective, n_trials=20)
+
+
+optuna.visualization.matplotlib.plot_optimization_history(cnn_study);
+optuna.visualization.matplotlib.plot_param_importances(cnn_study);
+optuna.visualization.matplotlib.plot_slice(cnn_study);
+
+
+cnn_acc = []
+cnn_pred = np.zeros((test_x.shape[0], 4))
+seed = 47
+skf = StratifiedKFold(n_splits=8, random_state=47, shuffle=True)
+es = EarlyStopping(monitor="val_acc", patience=15, mode="max", verbose=0)
+
+for i, (train_idx, val_idx) in enumerate(skf.split(train_x, train.target)):
+    print(f"{i+1} Fold Training.....")
+    x_train, y_train = train_x[train_idx], train_y[train_idx]
+    x_val, y_val = train_x[val_idx], train_y[val_idx]
+    
+    cnn = create_model(cnn_study.best_params['num_layer'], cnn_study.best_params['mid_units'], 
+                      [cnn_study.best_params[f'num_filter_{i}'] for i in range(cnn_study.best_params['num_layer'])])
+    
+    # ModelCheckpoint Fold마다 갱신
+    mc = ModelCheckpoint(f"model_{i+1}.h5", save_best_only=True, monitor="val_acc", mode="max", verbose=0)
+    
+    # 모델 Complie
+    optimizer = Adam(learning_rate=cnn_study.best_params['learning_rate'])
+    cnn.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["acc"])
+    cnn.fit(x_train, y_train, validation_data=(x_val, y_val), epochs=100, batch_size=32, callbacks=[es,mc], verbose=0)
+    
+    # 최고 성능 기록 모델
+    best = load_model(f"model_{i+1}.h5")
+    
+    val_pred = best.predict(x_val)
+    
+    val_cls = np.argmax(val_pred, axis=1)
+    
+    fold_cnn_acc = accuracy_score(np.argmax(y_val, axis=1), val_cls)
+    cnn_acc.append(fold_cnn_acc)
+    print(f"{i+1} Fold ACC of CNN = {fold_cnn_acc}\n")
+    
+    fold_pred = best.predict(test_x) / skf.n_splits
+    cnn_pred += fold_pred
+
+
+np.mean(cnn_acc)
+submission['target'] = np.argmax(cnn_pred, axis = 1)
+submission.to_csv(path + 'submission.csv', index = False)
+submission.target.value_counts()
+
+
